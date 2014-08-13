5f9a7ef5ed1f1b6fab13db42075b9023d33a42f6
chaqui <josue.chaqui@gmail.com>
2014-08-13 15:17:52 -0600

    agregando codigo de arduino

--- codigo_arduino/codigo_arduino.ino
+++ codigo_arduino/codigo_arduino.ino
@@ -1,17 +1,17 @@
-int LED=13;
+int LED=13; //variable del pin a seleccionar 
 void setup() {
-    pinMode(LED, OUTPUT);
-    Serial.begin(9600);
+    pinMode(LED, OUTPUT); //dejand el pin 13 como salida
+    Serial.begin(9600); //iniciamos la interfz serial 
 }
 
 void loop() {
     if (Serial.available()) {
-        char c = Serial.read();
-        if (c == 'H') {
-            digitalWrite(LED, HIGH);
+        char c = Serial.read(); //leemos la interfaz serial
+        if (c == 'H') { // si es h 
+            digitalWrite(LED, HIGH); //encendemos el led
         }
-        else if (c == 'L') {
-            digitalWrite(LED, LOW);
+        else if (c == 'L') { //si es L
+            digitalWrite(LED, LOW); //apagamos el led
         }
-    }
+    }d
 }
